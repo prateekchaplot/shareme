a74a348af699e134013454e03bf9d5c2643989cf
Prateek Chaplot <prateekchaplot96@gmail.com>
2023-02-10 09:55:23 +0530

    Add frontend with login

---
+++ frontend/src/components/Login.jsx
@@ -0,0 +1,56 @@
+import React from 'react'
+import { GoogleLogin } from '@react-oauth/google'
+import shareVideo from '../assets/share.mp4'
+import logo from '../assets/logowhite.png'
+
+const Login = () => {
+
+  const responseGoogle = (response) => {
+    console.log(response)
+    localStorage.setItem('user', JSON.stringify(response))
+    // const { name, googleId, imageUrl } =
+  }
+
+  return (
+    <div className='flex justify-start items-center flex-col h-screen'>
+      <div className='relative w-full h-full'>
+        <video
+          src={shareVideo}
+          type='video/mp4'
+          loop
+          controls={false}
+          muted
+          autoPlay
+          className='w-full h-full object-cover'
+        />
+
+        <div className="absolute flex flex-col justify-center items-center top-0 right-0 left-0 bottom-0 bg-blackOverlay">
+          <div className="p-5">
+            <img src={logo} width='130px' alt='logo' />
+          </div>
+
+          <div className="shadow-2xl">
+            <GoogleLogin
+              clientId=''
+              render={(renderProps) => (
+                <button
+                  type='button'
+                  className='bg-mainColor'
+                  onClick={renderProps.onClick}
+                  disabled={renderProps.disabled}
+                >
+                  Sign in with Google
+                </button>
+              )}
+              onSuccess={responseGoogle}
+              onError={responseGoogle}
+              cookiePolicy='single_host_origin'
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Login
